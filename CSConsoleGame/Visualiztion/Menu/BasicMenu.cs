b70d9fdfd2ecc8c08b483aa3c753a18bb2f3299b
Sleeper <Sleeper@WIN-9P5PRODUQ06>
2022-11-17 18:57:40 +0300

    Updates

---
+++ CSConsoleGame/Visualiztion/Menu/BasicMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Visualiztion.Menu
+{
+    public abstract class BasicMenu<T>
+    {
+        protected List<T> sourceList = new List<T>();
+
+        public BasicMenu(in List<T> list)
+        {
+            sourceList = list;
+        }
+
+        public abstract void DrawToConsole();
+    }
+}
